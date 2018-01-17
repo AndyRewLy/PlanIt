6fb1466d146b28b0f9a348cecdafcbc2810eb143
Andrew Ly <andyrewly@gmail.com>
2018-01-17 10:30:37 -0800

    Initial Project Setup

---
+++ fullstack_template/static/js/App.jsx
@@ -0,0 +1,9 @@
+//App.jsx
+
+import React from "react";
+
+export default class App extends React.Component {
+   render () {
+      return <div><h1>Plan It</h1><p> Hello React1!</p></div>;
+   }
+}
