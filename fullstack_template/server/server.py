74e491020398a47d7cccae49b6ac91c3c89d85c6
Andrew Ly <andyrewly@gmail.com>
2018-01-17 13:38:26 -0800

    Added a basic login backend/ui

--- fullstack_template/server/server.py
+++ fullstack_template/server/server.py
@@ -1,15 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, flash, redirect, render_template, request, session, abort
+import os
 
 app = Flask(__name__, static_folder="../static/dist", template_folder="../static")
 
 @app.route("/")
-def index():
-   return render_template("index.html")
+def home():
+   if not session.get("logged_in"):
+      return render_template("login.html")
+   else:
+      return render_template("index.html")
 
 @app.route("/hello")
 def hello():
    return "Hello World!"
 
+@app.route('/login', methods=['POST'])
+def do_admin_login():
+   if request.form['password'] == 'password' and request.form['username'] == 'admin':
+      session["logged_in"] = True
+   else:
+      flash('wrong password!')
+   return home()
 
 if __name__ == "__main__":
+   app.secret_key = os.urandom(12)
    app.run()
