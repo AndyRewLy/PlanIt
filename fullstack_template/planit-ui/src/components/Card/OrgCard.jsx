79727365bb1ffc50b34c900c5e0ce66cf3a34a08
andyrewly <andyrewly@gmail.com>
2018-05-11 01:56:41 -0700

    Made UI Changes for event dialogs and also homepage

--- fullstack_template/planit-ui/src/components/Card/OrgCard.jsx
+++ fullstack_template/planit-ui/src/components/Card/OrgCard.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+var default_img = "https://leanin-circles.s3.amazonaws.com/2014-10-23/1a3dca83-23cb-4d7f-9f77-08d4a92c84ef.jpg";
+
 require('./Card.css');
 class OrgCard extends React.Component {
     constructor(props){
@@ -15,9 +17,11 @@ class OrgCard extends React.Component {
         <div className="Card"
          onClick={() => this.props.renderOrgInfo(this.props.organizationId, this.props.canJoin)}>
             <div class="card-content">
-                <img class="card-sub-item" src= {this.props.organizationImage}></img>
-                <h1 class="card-sub-item">{this.props.organizationName}</h1>
-                <h2 class="card-sub-item">{this.props.organizationDescription}</h2>
+                <img class="card-sub-item" src= {this.props.organizationImage} onError={(e) => {e.target.src=default_img; e.target.className="card-sub-item-err"}}></img>
+                <div className="card-sub-container">
+                    <h1 class="card-sub-item">{this.props.organizationName}</h1>
+                    <h2 class="card-sub-item">{this.props.organizationDescription}</h2>
+                </div>
             </div>
         </div>);
       }
